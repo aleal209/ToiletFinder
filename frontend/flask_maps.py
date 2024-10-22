d3b72d2a466fc5b727347706536b21e014053415
Jasonjjtb <jasonbube2@gmail.com>
2024-10-22 09:23:11 -0700

    Updated

--- frontend/flask_maps.py
+++ frontend/flask_maps.py
@@ -3,9 +3,23 @@ from flask import request
 import arrow
 import config
 import logging
+import csv
+import pymongo
+from pymongo import MongoClient
 
 app = flask.Flask(__name__)
 CONFIG = config.configuration()
+
+cluster = MongoClient("mongodb+srv://dbUser:BananaLOL@toiletbuddy.dxyty.mongodb.net/?retryWrites=true&w=majority&appName=ToiletBuddy")
+db = cluster["ToiletBuddies"]
+toilet_collection = db["Toilets"]
+
+# csv_file_path = "tb.csv"
+#
+# with open(csv_file_path, mode='r') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         toilet_collection.insert_one(row)
 
 ### Pages ###
 @app.route("/")
