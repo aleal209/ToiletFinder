43e19f311b60d6ced53169ce262f0ceacd8d933a
Soosung Yang <soosungy@uoregon.edu>
2024-12-05 13:18:11 -0800

    back

--- frontend/flask_maps.py
+++ frontend/flask_maps.py
@@ -17,14 +17,15 @@ review_collection = db["Reviews"]
 user_collection = db["UserInfo"]
 
 @app.route("/")
-@app.route("/find-bathroom")
-def find_bathroom():
-    app.logger.debug("Main page entry")
-    return flask.render_template('map.html')
-
 @app.route("/index")
 def index():
+    app.logger.debug("Main page entry")
     return flask.render_template('login.html')
+
+@app.route("/find-bathroom")
+def find_bathroom():
+    return flask.render_template('map.html')
+
 
 @app.route("/add-bathroom")
 def add_bathroom():
